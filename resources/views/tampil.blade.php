c8236552e32442ca90806fd2bd9f20d92fcc1667
msalmanalghani17 <msalmanalghani17@gmail.com>
2023-11-03 17:21:18 +0700

    Mmeperbaiki bar

--- resources/views/tampil.blade.php
+++ resources/views/tampil.blade.php
@@ -8,14 +8,22 @@
 </head>
 <body>
     <x-navbar/>
-    <div class="flex justify-items-stretch">
+    {{-- <div class="flex justify-items-stretch">
     <div class="flex">
         <div class="w-1/4">
             <x-sidebar/>    
         </div>
     </div>
-    <x-halamantambahdata/>
-    <x-tabel/>    
+    </div> --}}
+    <div>
+        <x-halamantambahdata/>
+        <div class="p-2 m-3 border-spacing-8">
+            <x-tabel/>    
+        </div>
+    </div>
         
 </body>
+<div class="bottom-100">
+    <x-footer/>
+</div>
 </html>
